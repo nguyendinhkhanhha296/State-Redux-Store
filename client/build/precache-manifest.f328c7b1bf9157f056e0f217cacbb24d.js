b8bba33c73403b7e919a5e7d22ecf64fb13ab531
nguyendinhkhanhha296 <nguyendinhkhanhha296@gmail.com>
2022-11-18 04:10:40 -0800

    fix all errors related to invalid token

---
+++ client/build/precache-manifest.f328c7b1bf9157f056e0f217cacbb24d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "616d21e81a9f401f7ca38d9e25715dd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46a7654cd036226e5701",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "3fefea1a4c6ffdae1a6e",
+    "url": "/static/js/2.936ddb26.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.936ddb26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46a7654cd036226e5701",
+    "url": "/static/js/main.10fe9980.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.10fe9980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d52f44d6fb82598fed4",
+    "url": "/static/js/runtime-main.c62b8046.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
@@ -35,15 +35,15 @@ function App() {
         <div>
         <Provider store={store}>
           <Nav />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/orderHistory" component={OrderHistory} />
-            <Route exact path="/products/:id" component={Detail} />
-            <Route exact path="/success" component={Success} />
-            <Route component={NoMatch} />
-          </Switch>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signup" element={<Signup />} />
+            <Route exact path="/orderHistory" element={<OrderHistory />} />
+            <Route exact path="/products/:id" element={<Detail />} />
+            <Route exact path="/success" element={<Success />} />
+            <Route element={<NoMatch />} />
+          </Routes>
           </Provider>
         </div>
       </Router>
