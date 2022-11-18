03a95437fe5a4c607ddedc21279bdead5cb457d1
nguyendinhkhanhha296 <nguyendinhkhanhha296@gmail.com>
2022-11-18 03:06:30 -0800

    import useDispatch  and useSelector

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,40 +1,38 @@
 import React from 'react';
-import { useStoreContext } from "../../utils/GlobalState";
-import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 import { idbPromise } from "../../utils/helpers";
+import { useDispatch } from 'react-redux';
 
 const CartItem = ({ item }) => {
+    const dispatch = useDispatch(); 
 
-  const [, dispatch] = useStoreContext();
-
-  const removeFromCart = item => {
-    dispatch({
-      type: REMOVE_FROM_CART,
-      _id: item._id
-    });
-    idbPromise('cart', 'delete', { ...item });
-
-  };
-
-  const onChange = (e) => {
-    const value = e.target.value;
-    if (value === '0') {
+    const removeFromCart = item => {
       dispatch({
         type: REMOVE_FROM_CART,
         _id: item._id
       });
       idbPromise('cart', 'delete', { ...item });
+    };
 
-    } else {
-      dispatch({
-        type: UPDATE_CART_QUANTITY,
-        _id: item._id,
-        purchaseQuantity: parseInt(value)
-      });
-      idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
-
-    }
-  }
+    const onChange = (e) => {
+        const value = e.target.value;
+        if (value === '0') {
+          dispatch({
+            type: REMOVE_FROM_CART,
+            _id: item._id
+          });
+        
+          idbPromise('cart', 'delete', { ...item });
+        } else {
+          dispatch({
+            type: UPDATE_CART_QUANTITY,
+            _id: item._id,
+            purchaseQuantity: parseInt(value)
+          });
+        
+          idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
+        }
+      };
 
   return (
     <div className="flex-row">
@@ -53,14 +51,14 @@ const CartItem = ({ item }) => {
             placeholder="1"
             value={item.purchaseQuantity}
             onChange={onChange}
-          />
+            />
           <span
             role="img"
             aria-label="trash"
             onClick={() => removeFromCart(item)}
-          >
+            >
             🗑️
-          </span>
+         </span>
         </div>
       </div>
     </div>
--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from "../../utils/GlobalState";
-import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 import { idbPromise } from "../../utils/helpers";
+import { useSelector, useDispatch } from "react-redux";
 
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
-
   const {
     image,
     name,
@@ -16,7 +14,11 @@ function ProductItem(item) {
     quantity
   } = item;
 
-  const { cart } = state
+  const state = useSelector(state => state);
+
+  const dispatch = useDispatch();
+
+  const { cart } = state;
 
   const addToCart = () => {
     const itemInCart = cart.find((cartItem) => cartItem._id === _id)
